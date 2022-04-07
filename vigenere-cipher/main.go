13a8d1a5c516d9036788575fdafbb3fdd35d1c2a
Hanz <haniel56@zoho.eu>
2022-04-07 23:39:42 +0530

    fix: use utf8

--- vigenere-cipher/main.go
+++ vigenere-cipher/main.go
@@ -2,49 +2,61 @@
 * Exercise from the book "Get Programming with Go"
 * Unit 2
 * Ref: https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher#Algebraic_description
+* Implemented in UTF8 instead of ASCII.
  */
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"os"
+	"unicode/utf8"
+)
 
 /*
-* []byte to string
+* UTF8 def: https://www.rfc-editor.org/rfc/rfc3629#section-3
+* utf8 can represent 2^21 chars
+ */
+var NoOfCharsInUTF8 int = int(math.Pow(2, 21))
+
+/*
+* []rune to string
 * @return string
  */
-func stringFromByteArray(str []byte) string {
-	out := make([]rune, len(str))
+func stringFromRuneArray(str []rune) string {
+	//out := make([]rune, len(str))
 
-	for i := 0; i < len(str); i++ {
-		out[i] = rune(str[i])
-	}
+	//for i := 0; i < len(str); i++ {
+	//	out[i] = str[i]
+	//}
 
-	return string(out)
+	return string(str)
 }
 
 /*
-* string to []byte
-* @return []byte
+* string to []rune
+* @return []rune
  */
-func byteArrayFromString(str string) []byte {
-	out := make([]byte, len(str))
+func runeArrayFromString(str string) []rune {
+	//out := make([]rune, len(str))
 
-	for i := 0; i < len(str); i++ {
-		out[i] = byte(str[i])
-	}
+	//for i := 0; i < len(str); i++ {
+	//	out[i] = rune(str[i])
+	//}
 
-	return out
+	return []rune(str)
 }
 
 /*
 * E(Mi) = (Mi + K(i mod m)) mod l
 * M -> Message, K -> Key, m -> len(K), l -> len(A)
  */
-func encrypt(message string, key string) []byte {
-	encrypted := make([]byte, len(message))
+func encryptRuneArray(message []rune, key []rune) []rune {
+	encrypted := make([]rune, len(message))
 
 	for i := 0; i < len(message); i++ {
-		encrypted[i] = byte((rune(message[i]) + rune(key[i%len(key)])))
+		encrypted[i] = rune(int(message[i]) + int(key[i%len(key)])%NoOfCharsInUTF8)
 	}
 
 	return (encrypted)
@@ -54,22 +66,93 @@ func encrypt(message string, key string) []byte {
 * D(Ci) = (Ci - K(i mod m)) mod l
 * C -> Cipher, K -> Key, m -> len(K), l -> len(A)
  */
-func decrypt(cipher string, key string) []byte {
-	decrypted := make([]byte, len(cipher))
+func decryptRuneArray(cipher []rune, key []rune) []rune {
+	decrypted := make([]rune, len(cipher))
 
 	for i := 0; i < len(cipher); i++ {
-		decrypted[i] = byte((rune(cipher[i]) - rune(key[i%len(key)])))
+		decrypted[i] = rune(int(cipher[i]) - int(key[i%len(key)])%NoOfCharsInUTF8)
 	}
 
 	return (decrypted)
 }
 
+// encrypt
+func encrypt(message string, key string) string {
+	// split str into runes
+	runeMsg := runeArrayFromString(message)
+	runeKey := runeArrayFromString(key)
+
+	encRune := encryptRuneArray(runeMsg, runeKey)
+
+	// join back as string
+	return stringFromRuneArray(encRune)
+}
+
+// decrypt
+func decrypt(message string, key string) string {
+	// split into runes
+	runeCipher := runeArrayFromString(message)
+	runeKey := runeArrayFromString(key)
+
+	decRune := decryptRuneArray(runeCipher, runeKey)
+
+	// rune to str
+	return stringFromRuneArray(decRune)
+}
+
+// for debugging
+func printInASCII(text string) {
+	for i := 0; i < len(text); i++ {
+		fmt.Printf("%v -> %d -> %b \n", string(text[i]), text[i], text[i])
+	}
+}
+
+// for debugging
+func printInUTF8(text string) {
+	for len(text) > 0 {
+		//for i := 0; i < len(text); i++ {
+		char, size := utf8.DecodeRune([]byte(text))
+		fmt.Printf("%c -> %d -> %08b -> size: %v byte(s). \n", char, char, char, size)
+		text = text[size:]
+	}
+}
+
 func main() {
-	key := "Jonz-Is AName"
-	text := "Hanz Is A@B#C$- Chocolate."
-	fmt.Println(text, byteArrayFromString(text), stringFromByteArray(byteArrayFromString(text)))
-	text_enc := encrypt(text, key)
-	fmt.Println(stringFromByteArray(text_enc), text_enc)
-	text_dec := decrypt(stringFromByteArray(text_enc), key)
-	fmt.Println(stringFromByteArray(text_dec), text_dec)
+	sampleKey := "Lorem Isum ßåßˆøœ∆˚å∫ß∂˙∫∫ßåç≈"
+	sampleText := "Eg: Go says so, that! A@B#C$- aå 水目馬 ;). πø∑ß∂œ∫√∆¥"
+
+	if len(os.Args) > 1 {
+		key := os.Args[1]
+
+		if len(os.Args) == 2 {
+			fmt.Printf("Key: %v\nSample-Text: %v\n\n", key, sampleText)
+
+			text_enc := encrypt(sampleText, key)
+			fmt.Printf("Encrypted Text: %v\n", text_enc)
+
+			text_dec := decrypt(text_enc, key)
+			fmt.Printf("Decrypted Text: %v\n", text_dec)
+		} else {
+			str := os.Args[2]
+			fmt.Printf("Key: %v\n Text: %v\n\n", key, str)
+
+			text_enc := encrypt(str, key)
+			fmt.Printf("Encrypted Text: %v\n", text_enc)
+
+			text_dec := decrypt(text_enc, key)
+			fmt.Printf("Decrypted Text: %v\n", text_dec)
+		}
+
+	} else {
+		fmt.Println("usage:\n$ vigenere-cipher KEY MESSAGE\nsample test case below:\n")
+
+		fmt.Printf("Sample-Key: %v\nSample-Text: %v\n\n", sampleKey, sampleText)
+
+		text_enc := encrypt(sampleText, sampleKey)
+		fmt.Printf("Encrypted Text: %v\n", text_enc)
+
+		text_dec := decrypt(text_enc, sampleKey)
+		fmt.Printf("Decrypted Text: %v\n", text_dec)
+	}
+
 }
